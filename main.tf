e78752dd6752244368961d2a065caf778675f6e8
Nhuengzii <32nhueng@gmail.com>
2023-05-06 20:37:22 +0700

    create botio_rest_api basic path `/{shop_id}/`

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+resource "aws_api_gateway_rest_api" "botio_rest_api" {
+  name        = "botio_rest_api"
+  description = "Api endpoint for interacting with botio"
+}
+
+resource "aws_api_gateway_resource" "shop_id" {
+  rest_api_id = aws_api_gateway_rest_api.botio_rest_api.id
+  parent_id   = aws_api_gateway_rest_api.botio_rest_api.root_resource_id
+  path_part   = "{shop_id}"
+}
