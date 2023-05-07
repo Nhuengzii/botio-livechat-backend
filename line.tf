4aa6a350b2b2b2d9f4a46d94003b855209b8fa4b
Nhuengzii <32nhueng@gmail.com>
2023-05-07 22:13:02 +0700

    create post method and proxy to its lambda for send line message

--- line.tf
+++ line.tf
@@ -16,12 +16,39 @@ resource "aws_api_gateway_resource" "line_webhook" {
   path_part   = "webhook"
 }
 
+resource "aws_api_gateway_resource" "line_conversation" {
+  rest_api_id = aws_api_gateway_rest_api.botio_rest_api.id
+  parent_id   = aws_api_gateway_resource.line_page_id.id
+  path_part   = "conversations"
+}
+
+resource "aws_api_gateway_resource" "line_conversation_id" {
+  rest_api_id = aws_api_gateway_rest_api.botio_rest_api.id
+  parent_id   = aws_api_gateway_resource.line_conversation.id
+  path_part   = "{conversation_id}"
+}
+
+resource "aws_api_gateway_resource" "line_message" {
+  rest_api_id = aws_api_gateway_rest_api.botio_rest_api.id
+  parent_id   = aws_api_gateway_resource.line_conversation_id.id
+  path_part   = "messages"
+}
+
+
 resource "aws_api_gateway_method" "post_validate_line_webhook" {
   rest_api_id   = aws_api_gateway_rest_api.botio_rest_api.id
   resource_id   = aws_api_gateway_resource.line_webhook.id
   authorization = "NONE"
   http_method   = "POST"
 }
+
+resource "aws_api_gateway_method" "post_line_message" {
+  rest_api_id   = aws_api_gateway_rest_api.botio_rest_api.id
+  resource_id   = aws_api_gateway_resource.line_message.id
+  authorization = "NONE"
+  http_method   = "POST"
+}
+
 
 resource "aws_api_gateway_integration" "post_validate_line_webhook" {
   http_method             = aws_api_gateway_method.post_validate_line_webhook.http_method
@@ -31,6 +58,17 @@ resource "aws_api_gateway_integration" "post_validate_line_webhook" {
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.validate_line_webhook_handler.invoke_arn
 }
+
+resource "aws_api_gateway_integration" "post_line_message" {
+  http_method             = aws_api_gateway_method.post_line_message.http_method
+  resource_id             = aws_api_gateway_resource.line_message.id
+  rest_api_id             = aws_api_gateway_rest_api.botio_rest_api.id
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.post_line_message_handler.invoke_arn
+}
+
+
 resource "aws_lambda_permission" "validate_line_webhook_handler_allow_execution_from_api_gateway" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
@@ -38,6 +76,16 @@ resource "aws_lambda_permission" "validate_line_webhook_handler_allow_execution_
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.botio_rest_api.execution_arn}/*/*/*"
 }
+
+resource "aws_lambda_permission" "post_line_message_handler_allow_execution_from_api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.post_line_message_handler.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.botio_rest_api.execution_arn}/*/*/*"
+}
+
+
 
 resource "aws_sqs_queue" "line_webhook_to_standardize_line_webhook_handler" {
   name = "line_webhook_to_standardize_facebook_webhook_handler"
@@ -66,6 +114,16 @@ resource "null_resource" "build_standardize_line_webhook_handler" {
   }
 }
 
+resource "null_resource" "build_post_line_message_handler" {
+  triggers = {
+    source_code_hash = filebase64sha256("post_line_message_handler/src/main.go")
+  }
+  provisioner "local-exec" {
+    command = "CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -C ./post_line_message_handler/src/ -o ../bin/main ."
+  }
+}
+
+
 resource "null_resource" "watch_validate_line_webhook_handler" {
   triggers = {
     validate_line_webhook_handler = aws_lambda_function.validate_line_webhook_handler.qualified_arn
@@ -79,6 +137,14 @@ resource "null_resource" "watch_standardize_line_webhook_handler" {
   depends_on = [null_resource.build_validate_line_webhook_handler]
 }
 
+resource "null_resource" "watch_post_line_message_handler" {
+  triggers = {
+    post_line_message_handler = aws_lambda_function.post_line_message_handler.qualified_arn
+  }
+  depends_on = [null_resource.build_post_line_message_handler]
+}
+
+
 data "archive_file" "validate_line_webhook_handler" {
   type        = "zip"
   source_dir  = "validate_line_webhook_handler"
@@ -89,6 +155,12 @@ data "archive_file" "standardize_line_webhook_handler" {
   type        = "zip"
   source_dir  = "standardize_line_webhook_handler"
   output_path = "standardize_line_webhook_handler/standardize_line_webhook_handler.zip"
+}
+
+data "archive_file" "post_line_message_handler" {
+  type        = "zip"
+  source_dir  = "post_line_message_handler"
+  output_path = "post_line_message_handler/post_line_message_handler.zip"
 }
 
 resource "aws_lambda_function" "validate_line_webhook_handler" {
@@ -122,4 +194,14 @@ resource "aws_lambda_function" "standardize_line_webhook_handler" {
     }
   }
   depends_on = [data.archive_file.standardize_line_webhook_handler]
+}
+
+resource "aws_lambda_function" "post_line_message_handler" {
+  filename         = data.archive_file.post_line_message_handler.output_path
+  function_name    = "post_line_message_handler"
+  role             = aws_iam_role.assume_role_lambda.arn
+  handler          = "main"
+  runtime          = "go1.x"
+  source_code_hash = filebase64sha256("post_line_message_handler/src/main.go")
+  depends_on       = [data.archive_file.post_line_message_handler]
 }
---
+++ post_line_message_handler/src/go.mod
@@ -0,0 +1,3 @@
+module post_line_message_handler
+
+go 1.20
