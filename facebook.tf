b85d262593c80302eab48f51f58fd1ead55f7b44
Nhuengzii <32nhueng@gmail.com>
2023-05-07 15:18:58 +0700

    create lambda handler for standardize message from facebook webhook after sqs

--- facebook.tf
+++ facebook.tf
@@ -41,6 +41,17 @@ resource "aws_lambda_function" "validate_facebook_webhook_handler" {
   source_code_hash = filebase64sha256("validate_facebook_webhook_handler/src/main.go")
   depends_on       = [data.archive_file.validate_facebook_webhook_handler]
 }
+
+resource "aws_lambda_function" "standardize_facebook_webhook_handler" {
+  filename         = "standardize_facebook_webhook_handler/standardize_facebook_webhook_handler.zip"
+  function_name    = "standardize_facebook_webhook_handler"
+  role             = aws_iam_role.assume_role_lambda.arn
+  handler          = "main"
+  runtime          = "go1.x"
+  source_code_hash = filebase64sha256("standardize_facebook_webhook_handler/src/main.go")
+  depends_on       = [data.archive_file.standardize_facebook_webhook_handler]
+}
+
 resource "aws_lambda_permission" "validate_facebook_webhook_handler_allow_execution_from_api_gateway" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
@@ -58,11 +69,27 @@ resource "null_resource" "build_validate_facebook_webhook_handler" {
   }
 }
 
+resource "null_resource" "build_standardize_facebook_webhook_handler" {
+  triggers = {
+    source_code_hash = "${filebase64sha256("standardize_facebook_webhook_handler/src/main.go")}"
+  }
+  provisioner "local-exec" {
+    command = "CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -C ./standardize_facebook_webhook_handler/src/ -o ../bin/main ."
+  }
+}
+
 data "archive_file" "validate_facebook_webhook_handler" {
   type        = "zip"
   source_file = "./validate_facebook_webhook_handler/bin/main"
   output_path = "./validate_facebook_webhook_handler/validate_facebook_webhook_handler.zip"
   depends_on  = [null_resource.build_validate_facebook_webhook_handler]
+}
+
+data "archive_file" "standardize_facebook_webhook_handler" {
+  type        = "zip"
+  source_file = "./standardize_facebook_webhook_handler/src/main.go"
+  output_path = "./standardize_facebook_webhook_handler/standardize_facebook_webhook_handler.zip"
+  depends_on  = [null_resource.build_standardize_facebook_webhook_handler]
 }
 
 
@@ -72,4 +99,11 @@ resource "null_resource" "watch_validate_facebook_webhook_handler" {
     source_code_hash = filebase64sha256("validate_facebook_webhook_handler/src/main.go")
   }
   depends_on = [null_resource.build_validate_facebook_webhook_handler]
+}
+
+resource "null_resource" "watch_standardize_facebook_webhook_handler" {
+  triggers = {
+    source_code_hash = filebase64sha256("standardize_facebook_webhook_handler/src/main.go")
+  }
+  depends_on = [null_resource.build_standardize_facebook_webhook_handler]
 }
