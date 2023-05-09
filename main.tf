fa5b86822db6fe579efd47a3d8b6745c9a49b658
Nhuengzii <32nhueng@gmail.com>
2023-05-09 23:32:13 +0700

    add sns full access permission to every lambda

--- main.tf
+++ main.tf
@@ -49,6 +49,10 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_sqsexecution_to_assume_r
   role       = aws_iam_role.assume_role_lambda.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
 }
+resource "aws_iam_role_policy_attachment" "lambda_basic_snsexecution_to_assume_role_lambda" {
+  role       = aws_iam_role.assume_role_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
+}
 
 resource "aws_api_gateway_deployment" "botio_rest_api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.botio_rest_api.id
