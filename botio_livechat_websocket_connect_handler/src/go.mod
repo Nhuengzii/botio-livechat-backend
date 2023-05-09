f483fa9d820039ec0359d1374614b62c3449b688
Nhuengzii <32nhueng@gmail.com>
2023-05-09 19:33:20 +0700

    basic botio_livechat_websocket_connect_handler
    print when activate and return 200

--- botio_livechat_websocket_connect_handler/src/go.mod
+++ botio_livechat_websocket_connect_handler/src/go.mod
@@ -1,3 +1,5 @@
 module botio_livechat_websocket_connect_handler
 
 go 1.20
+
+require github.com/aws/aws-lambda-go v1.41.0
--- botio_livechat_websocket_connect_handler/src/main.go
+++ botio_livechat_websocket_connect_handler/src/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println("Got connect")
+	lambda.Start(Handler)
+}
+
+func Handler(ctx context.Context, request events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
+	connectionID := request.RequestContext.ConnectionID
+	log.Println("Livechat websocket connect lambda handler")
+	log.Println("ConnectionID: ", connectionID)
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+	}, nil
 }
