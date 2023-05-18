a7e782f0bf143acce6938fa34d3ba8925559a481
Nhuengzii <32nhueng@gmail.com>
2023-05-18 21:42:59 +0700

    save connectionId to redis when got connection `shopId:connectionId=1`

--- botio_livechat_websocket_connect_handler/src/main.go
+++ botio_livechat_websocket_connect_handler/src/main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/go-redis/redis/v8"
 )
 
 func main() {
@@ -17,13 +18,9 @@ func main() {
 	lambda.Start(Handler)
 }
 
-func discordLog(contents []string) {
+func discordLog(content string) {
 	webhookURL := "https://discord.com/api/webhooks/1108750713758175293/U96dYkOWsQYSYrCx6rCFPGvrJ7TY_tMMVmm5IWIAdsCM7ffi_Fa-W9Dfxt7dAd8WNYR2"
-	join_contents := ""
-	for _, content := range contents {
-		join_contents += content + "\n"
-	}
-	payload := map[string]string{"content": join_contents}
+	payload := map[string]string{"content": content}
 	json_payload, _ := json.Marshal(payload)
 	_, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(json_payload))
 	if err != nil {
@@ -33,8 +30,19 @@ func discordLog(contents []string) {
 
 func Handler(ctx context.Context, request events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
 	connectionID := request.RequestContext.ConnectionID
-	contents := []string{fmt.Sprintf("Got connect: %s", connectionID)}
-	discordLog(contents)
+	shopId := request.QueryStringParameters["shopId"]
+	discordLog(fmt.Sprint("Got connect: ", connectionID))
+	my_ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "redis-15520.c252.ap-southeast-1-1.ec2.cloud.redislabs.com:15520",
+		Password: "dcesPhFIPwWrItb2yaNe5UT0sbhv9FJk",
+	})
+
+	err := rdb.Set(my_ctx, shopId+":"+connectionID, 1, 0).Err()
+	if err != nil {
+		discordLog(fmt.Sprint("Error setting current connection: ", err))
+	}
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 	}, nil
