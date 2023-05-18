d2fd6fabe7551954ffdc54809395621e533a3768
Nhuengzii <32nhueng@gmail.com>
2023-05-18 20:49:31 +0700

    create utility for discord log in websocket connect handler

--- botio_livechat_websocket_connect_handler/src/main.go
+++ botio_livechat_websocket_connect_handler/src/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"log"
 )
 
 func main() {
@@ -13,10 +17,24 @@ func main() {
 	lambda.Start(Handler)
 }
 
+func discordLog(contents []string) {
+	webhookURL := "https://discord.com/api/webhooks/1108750713758175293/U96dYkOWsQYSYrCx6rCFPGvrJ7TY_tMMVmm5IWIAdsCM7ffi_Fa-W9Dfxt7dAd8WNYR2"
+	join_contents := ""
+	for _, content := range contents {
+		join_contents += content + "\n"
+	}
+	payload := map[string]string{"content": join_contents}
+	json_payload, _ := json.Marshal(payload)
+	_, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(json_payload))
+	if err != nil {
+		log.Println("Error sending discord log: ", err)
+	}
+}
+
 func Handler(ctx context.Context, request events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
 	connectionID := request.RequestContext.ConnectionID
-	log.Println("Livechat websocket connect lambda handler")
-	log.Println("ConnectionID: ", connectionID)
+	contents := []string{fmt.Sprintf("Got connect: %s", connectionID)}
+	discordLog(contents)
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 	}, nil
