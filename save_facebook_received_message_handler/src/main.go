6b6f1e336441f11a966ec3724bcbbdaaf631a6d6
PAFFx <jj2jom@gmail.com>
2023-05-18 15:43:16 +0700

    connect to database

--- save_facebook_received_message_handler/src/main.go
+++ save_facebook_received_message_handler/src/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+const uri = "mongodb+srv://paff:thisispassword@botiolivechat.nevtjex.mongodb.net/?retryWrites=true&w=majority"
 
 func main() {
 	lambda.Start(handle)
@@ -14,6 +21,31 @@ func main() {
 
 func handle(ctx context.Context, sqsEvent events.SQSEvent) {
 	log.Println("facebook database handler")
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	opts := options.Client().ApplyURI(uri)
+
+	// create a new client and connect to the server
+	client, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		log.Println("Error connecting to mongo atlas : ", err)
+		return
+	}
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			log.Println("Error disconnecting from mongo atlas : ", err)
+			return
+		}
+	}()
+
+	// ping
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		log.Println("Error Pinging DB : ", err)
+		return
+	}
+	log.Println("Successfully connect to MongoDB")
 	for _, record := range sqsEvent.Records {
 		log.Println(record)
 	}
