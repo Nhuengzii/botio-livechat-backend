12c3595fe217791d5bd59207541da59ca0d2d5e2
PAFFx <jj2jom@gmail.com>
2023-05-19 16:10:29 +0700

    create output_message_struct.go

---
+++ get_facebook_conversation_handler/src/output_message_struct.go
@@ -0,0 +1,22 @@
+package main
+
+type OutputMessage struct {
+	Conversations []Conversation `json:"conversations"`
+}
+
+type Conversation struct {
+	ConversationID  string        `json:"conversationID"`
+	ConversationPic Payload       `json:"conversationPic"`
+	UpdatedTime     int64         `json:"updatedTime"`
+	Participants    []Participant `json:"participants"`
+	LastActivity    string        `json:"lastActivity"`
+}
+
+type Participant struct {
+	UserID     string  `json:"userID"`
+	ProfilePic Payload `json:"profilePic"`
+	Username   string  `json:"username"`
+}
+type Payload struct {
+	Src string `json:"src"`
+}
