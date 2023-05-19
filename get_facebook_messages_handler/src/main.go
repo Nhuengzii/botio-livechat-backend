af3d325d425d7ce58aab87aec1c1f55000551f18
PAFFx <jj2jom@gmail.com>
2023-05-19 18:28:28 +0700

    return json byte messageOutput body

--- get_facebook_messages_handler/src/main.go
+++ get_facebook_messages_handler/src/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -14,13 +17,35 @@ func main() {
 
 func handler(context context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	discordLog("facebook get messages handler!!!")
+	start := time.Now()
 
 	pathParams := request.PathParameters
 	// shopID := pathParams["shop_id"]
 	pageID := pathParams["page_id"]
 	conversationID := pathParams["conversation_id"]
 
+	var outputMessage OutputMessage
+	err := QueryMessages(pageID, conversationID, &outputMessage)
+	if err != nil {
+		discordLog(fmt.Sprint(err))
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadGateway,
+		}, err
+	}
+
+	jsonBodyByte, err := json.Marshal(outputMessage)
+	jsonString := string(jsonBodyByte)
+	if err != nil {
+		discordLog(fmt.Sprint(err))
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadGateway,
+		}, err
+	}
+
+	discordLog(fmt.Sprintf("Total Elasped time: %v", time.Since(start)))
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
+		Body:       jsonString,
 	}, nil
 }
