51bc4c868b6c8f3d76e700b3defb68e5d5cb714a
PAFFx <jj2jom@gmail.com>
2023-05-19 16:30:41 +0700

    query for matching pageID

--- get_facebook_conversation_handler/src/main.go
+++ get_facebook_conversation_handler/src/main.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"context"
-	"log"
-	"time"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-const uri = "mongodb+srv://paff:thisispassword@botiolivechat.qsb7kv4.mongodb.net/?retryWrites=true&w=majority"
 
 func main() {
 	lambda.Start(handler)
@@ -20,36 +14,14 @@ func main() {
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) {
 	discordLog("get_facebook_conversation handler!!!")
-	start := time.Now()
 
 	pathParams := request.PathParameters
-	shopID := pathParams["shop_id"]
+	// shopID := pathParams["shop_id"]
 	pageID := pathParams["page_id"]
-	conversationID := pathParams["conversation_id"]
 
-	ctx, cancel := context.WithTimeout(context.Background(), 1500*time.Millisecond)
-	defer cancel()
-
-	opts := options.Client().ApplyURI(uri)
-
-	// create a new client and connect to the server
-	client, err := mongo.Connect(ctx, opts)
+	var outputMessage OutputMessage
+	err := QueryConversations(pageID, &outputMessage)
 	if err != nil {
-		log.Println("Error connecting to mongo atlas : ", err)
-		return
+		discordLog(fmt.Sprint(err))
 	}
-
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			log.Println("Error disconnecting from mongo atlas : ", err)
-			return
-		}
-	}()
-
-	// ping
-	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Err(); err != nil {
-		log.Println("Error Pinging DB : ", err)
-		return
-	}
-	log.Println("Successfully connect to MongoDB ", time.Since(start))
 }
--- get_facebook_conversation_handler/src/query_conversations.go
+++ get_facebook_conversation_handler/src/query_conversations.go
@@ -11,7 +11,9 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func QueryConversations() error {
+const uri = "mongodb+srv://paff:thisispassword@botiolivechat.qsb7kv4.mongodb.net/?retryWrites=true&w=majority"
+
+func QueryConversations(pageID string, outputMessage *OutputMessage) error {
 	start := time.Now()
 	ctx, cancel := context.WithTimeout(context.Background(), 1500*time.Millisecond)
 	defer cancel()
@@ -38,7 +40,16 @@ func QueryConversations() error {
 		return err
 	}
 	log.Println("Successfully connect to MongoDB ", time.Since(start))
-	discordLog(fmt.Sprint("Successfully connect to mongo Elasped : %v", time.Since(start)))
+	discordLog(fmt.Sprintf("Successfully connect to mongo Elasped : %v", time.Since(start)))
+
+	// start query
+	coll := client.Database("BotioLivechat").Collection("facebook_conversations")
+	filter := bson.D{{"pageID", pageID}}
+	cur, err := coll.Find(ctx, filter)
+	if err != nil {
+		discordLog(fmt.Sprintf("Error query with filter pageID:%v Error : %v", pageID, err))
+		return err
+	}
 
 	return nil
 }
