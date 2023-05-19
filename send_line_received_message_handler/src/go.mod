bb54b4c23342fcddd8ea2e2b5b9058d1ab9cf483
Nhuengzii <32nhueng@gmail.com>
2023-05-19 11:23:52 +0700

    basic implement send_line_received_message_handler
    just log event to discord

--- send_line_received_message_handler/src/go.mod
+++ send_line_received_message_handler/src/go.mod
@@ -1,3 +1,5 @@
 module send_line_received_message_handler
 
 go 1.20
+
+require github.com/aws/aws-lambda-go v1.41.0
--- send_line_received_message_handler/src/main.go
+++ send_line_received_message_handler/src/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
 func main() {
 	fmt.Println("Hello Me")
+	lambda.Start(Handler)
+}
+
+func discordLog(content string) {
+	webhookURL := "https://discord.com/api/webhooks/1108750713758175293/U96dYkOWsQYSYrCx6rCFPGvrJ7TY_tMMVmm5IWIAdsCM7ffi_Fa-W9Dfxt7dAd8WNYR2"
+	payload := map[string]string{"content": content}
+	json_payload, _ := json.Marshal(payload)
+	_, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(json_payload))
+	if err != nil {
+		log.Println("Error sending discord log: ", err)
+	}
+}
+func Handler(ctx context.Context, sqsEvent events.SQSEvent) {
+
+	// log detailed info about this event with discordlog
+	discordLog(fmt.Sprint("Got event: ", sqsEvent))
+
+	for _, message := range sqsEvent.Records {
+		fmt.Println("Got message: ", message.Body)
+		body_pretty, _ := json.MarshalIndent(message.Body, "", "  ")
+		discordLog(fmt.Sprint("Got message: ", string(body_pretty)))
+	}
 }
