1829d5cc9b0bbf96833ca11be87774f2e2dfb75e
PAFFx <jj2jom@gmail.com>
2023-05-19 16:51:54 +0700

    change error gateway return status to bad gateway

--- get_facebook_conversation_handler/src/main.go
+++ get_facebook_conversation_handler/src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,7 +13,7 @@ func main() {
 	lambda.Start(handler)
 }
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) {
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
 	discordLog("get_facebook_conversation handler!!!")
 
 	pathParams := request.PathParameters
@@ -23,8 +24,15 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) {
 	err := QueryConversations(pageID, &outputMessage)
 	if err != nil {
 		discordLog(fmt.Sprint(err))
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadGateway,
+		}
 	}
 	for _, conversation := range outputMessage.Conversations {
 		discordLog(fmt.Sprintf("Last message in conversation %v is %v", conversation.ConversationID, conversation.LastActivity))
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
 	}
 }
