3f0d11099ed63a9c424e6112ea2b6dd8ae0a0b54
Nhuengzii <32nhueng@gmail.com>
2023-05-19 18:23:41 +0700

    implement basic websocket_default_handler to send simple text back to connection

--- botio_livechat_websocket_default_handler/src/go.mod
+++ botio_livechat_websocket_default_handler/src/go.mod
@@ -1,3 +1,26 @@
 module botio_livechat_websocket_default_handler
 
 go 1.20
+
+require (
+	github.com/aws/aws-lambda-go v1.41.0
+	github.com/aws/aws-sdk-go-v2 v1.18.0
+	github.com/aws/aws-sdk-go-v2/config v1.18.25
+	github.com/aws/aws-sdk-go-v2/service/apigatewaymanagementapi v1.11.10
+	github.com/go-redis/redis/v8 v8.11.5
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.24 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.33 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.27 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.34 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.27 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.12.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.19.0 // indirect
+	github.com/aws/smithy-go v1.13.5 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
--- botio_livechat_websocket_default_handler/src/main.go
+++ botio_livechat_websocket_default_handler/src/main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/apigatewaymanagementapi"
+)
 
 func main() {
-	fmt.Println("Hello Not Me")
+	fmt.Println("Got connect")
+	lambda.Start(Handler)
+}
+
+func discordLog(content string) {
+	webhookURL := "https://discord.com/api/webhooks/1108750713758175293/U96dYkOWsQYSYrCx6rCFPGvrJ7TY_tMMVmm5IWIAdsCM7ffi_Fa-W9Dfxt7dAd8WNYR2"
+	payload := map[string]string{"content": content}
+	json_payload, _ := json.Marshal(payload)
+	_, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(json_payload))
+	if err != nil {
+		log.Println("Error sending discord log: ", err)
+	}
+}
+
+func Handler(ctx context.Context, request events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
+	connectionID := request.RequestContext.ConnectionID
+	shopId := request.QueryStringParameters["shopId"]
+	discordLog(fmt.Sprint("Got connect: ", connectionID))
+	my_ctx := context.Background()
+	if err != nil {
+		discordLog(fmt.Sprint("Error setting current connection: ", err))
+	}
+	endpoint := os.Getenv("WEBSOCKET_API_ENDPOINT")
+	discordLog(fmt.Sprint("Endpoint: ", endpoint))
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-southeast-1"))
+	if err != nil {
+		discordLog(fmt.Sprint("Error loading config: ", err))
+	}
+	svc := apigatewaymanagementapi.NewFromConfig(cfg, func(o *apigatewaymanagementapi.Options) {
+		o.EndpointResolver = apigatewaymanagementapi.EndpointResolverFunc(func(region string, options apigatewaymanagementapi.EndpointResolverOptions) (aws.Endpoint, error) {
+			return aws.Endpoint{
+				URL:           endpoint,
+				SigningRegion: region,
+			}, nil
+		})
+	})
+	input := &apigatewaymanagementapi.PostToConnectionInput{ConnectionId: aws.String(connectionID), Data: []byte("Hello this is from Default handler")}
+	_, err = svc.PostToConnection(context.Background(), input)
+	if err != nil {
+		discordLog(fmt.Sprint("Error sending message: ", err))
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+	}, nil
 }
