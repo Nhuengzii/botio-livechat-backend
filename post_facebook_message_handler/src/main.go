ee221caed5588972b04b45b1476290dd753eb424
PAFFx <jj2jom@gmail.com>
2023-05-22 18:19:04 +0700

    add time log

--- post_facebook_message_handler/src/main.go
+++ post_facebook_message_handler/src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -13,7 +14,9 @@ func main() {
 }
 
 func handler(context context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	start := time.Now()
 	discordLog(fmt.Sprint("-------Post-FacebookMessage-handler!!!!"))
 
+	discordLog(fmt.Sprintf("Elasped : %v", time.Since(start)))
 	return events.APIGatewayProxyResponse{}, nil
 }
