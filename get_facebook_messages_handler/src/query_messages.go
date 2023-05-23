1469c4e90b187bc017003fe69464d8baa922721a
PAFFx <jj2jom@gmail.com>
2023-05-23 14:29:00 +0700

    refactor connect mongo to function

--- get_facebook_messages_handler/src/query_messages.go
+++ get_facebook_messages_handler/src/query_messages.go
@@ -3,44 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const uri = "mongodb+srv://paff:thisispassword@botiolivechat.qsb7kv4.mongodb.net/?retryWrites=true&w=majority"
 
-func QueryMessages(pageID string, conversationID string, outputMessage *OutputMessage) error {
-	start := time.Now()
+func QueryMessages(client *mongo.Client, pageID string, conversationID string, outputMessage *OutputMessage) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 1500*time.Millisecond)
 	defer cancel()
-
-	opts := options.Client().ApplyURI(uri)
-
-	// create a new client and connect to the server
-	client, err := mongo.Connect(ctx, opts)
-	if err != nil {
-		log.Println("Error connecting to mongo atlas : ", err)
-		return err
-	}
-
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			log.Println("Error disconnecting from mongo atlas : ", err)
-			return
-		}
-	}()
-
-	// ping
-	if err := client.Database("admin").RunCommand(ctx, bson.D{{Key: "ping", Value: 1}}).Err(); err != nil {
-		log.Println("Error Pinging DB : ", err)
-		return err
-	}
-	log.Println("Successfully connect to MongoDB ", time.Since(start))
-	discordLog(fmt.Sprintf("Successfully connect to mongo Elasped : %v", time.Since(start)))
 
 	// start query
 	coll := client.Database("BotioLivechat").Collection("facebook_messages")
