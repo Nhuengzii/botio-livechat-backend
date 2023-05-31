f1a0122c1f11d3b925da5b05b34f6a3ad5a3f041
Nhuengzii <32nhueng@gmail.com>
2023-05-31 16:52:18 +0700

    implement `send_line_received_message_handler`

--- send_line_received_message_handler/src/main.go
+++ send_line_received_message_handler/src/main.go
@@ -7,14 +7,82 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/apigatewaymanagementapi"
+	"github.com/go-redis/redis/v8"
 )
 
 func main() {
 	fmt.Println("Hello Me")
 	lambda.Start(Handler)
+}
+func sendMessage(svc *apigatewaymanagementapi.Client, connectionID string, message string) {
+	input := &apigatewaymanagementapi.PostToConnectionInput{ConnectionId: aws.String(connectionID), Data: []byte(message)}
+	_, err := svc.PostToConnection(context.Background(), input)
+	if err != nil {
+		discordLog(fmt.Sprint("Error sending message: ", err))
+	}
+}
+
+type LineReceivedMessage struct {
+	ShopID         string `json:"shopID"`
+	Platform       string `json:"platform"`
+	PageID         string `json:"pageID"`
+	ConversationID string `json:"conversationID"`
+	MessageID      string `json:"messageID"`
+	Timestamp      int64  `json:"timestamp"`
+	Source         struct {
+		UserID   string `json:"userID"`
+		UserType string `json:"userType"`
+	}
+	Message     string `json:"message"`
+	Attachments []struct {
+		AttachmentType string `json:"attachmentType"`
+		Payload        struct {
+			Src string `json:"src"`
+		}
+	}
+}
+
+func getRedisClient() *redis.Client {
+	redis_addr := os.Getenv("REDIS_ACCESS_ADDR")
+	redis_password := os.Getenv("REDIS_ACCESS_PASSWORD")
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redis_addr,
+		Password: redis_password,
+	})
+	return rdb
+}
+
+func getSVCClient() *apigatewaymanagementapi.Client {
+	endpoint := os.Getenv("WEBSOCKET_API_ENDPOINT")
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-southeast-1"))
+	if err != nil {
+		discordLog(fmt.Sprint("Error loading config: ", err))
+	}
+	svc := apigatewaymanagementapi.NewFromConfig(cfg, func(o *apigatewaymanagementapi.Options) {
+		o.EndpointResolver = apigatewaymanagementapi.EndpointResolverFunc(func(region string, options apigatewaymanagementapi.EndpointResolverOptions) (aws.Endpoint, error) {
+			return aws.Endpoint{
+				URL:           endpoint,
+				SigningRegion: region,
+			}, nil
+		})
+	})
+	return svc
+}
+
+type EventRecordBody struct {
+	Message string `json:"Message"`
+}
+type WebsocketMessage struct {
+	Action  string `json:"action"`
+	Message string `json:"message"`
 }
 
 func discordLog(content string) {
@@ -27,13 +95,30 @@ func discordLog(content string) {
 	}
 }
 func Handler(ctx context.Context, sqsEvent events.SQSEvent) {
-
-	// log detailed info about this event with discordlog
-	discordLog(fmt.Sprint("Got event: ", sqsEvent))
-
+	rdb := getRedisClient()
+	svc := getSVCClient()
+	keys, err := rdb.Keys(ctx, "1:*").Result()
+	if err != nil {
+		discordLog(fmt.Sprint("Error getting keys from redis: ", err))
+	}
+	defer rdb.Close()
 	for _, message := range sqsEvent.Records {
-		fmt.Println("Got message: ", message.Body)
-		body_pretty, _ := json.MarshalIndent(message.Body, "", "  ")
-		discordLog(fmt.Sprint("Got message: ", string(body_pretty)))
+		var eventRec EventRecordBody
+		err := json.Unmarshal([]byte(message.Body), &eventRec)
+		if err != nil {
+			discordLog(fmt.Sprint("Error unmarshalling message in sendLineReceivedMessage: ", err))
+		}
+		var lineRM LineReceivedMessage
+		err = json.Unmarshal([]byte(eventRec.Message), &lineRM)
+		if err != nil {
+			discordLog(fmt.Sprint("Error unmarshalling message in sendLineReceivedMessage: ", err))
+		}
+		var wsMessage WebsocketMessage
+		wsMessage.Action = "userMessage"
+		wsMessage.Message = eventRec.Message
+		wsMessageJSON, _ := json.Marshal(wsMessage)
+		for _, key := range keys {
+			sendMessage(svc, strings.Split(key, ":")[1], string(wsMessageJSON))
+		}
 	}
 }
