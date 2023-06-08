f2adbf95cf1d1dc9af1718edc47afed6f7ee738d
PAFF <jj2jom@gmail.com>
2023-06-09 00:58:13 +0700

    facebook save_recieved_message unmarshal data

--- cmd/lambda/facebook/save_received_message/main.go
+++ cmd/lambda/facebook/save_received_message/main.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"os"
 
 	"github.com/Nhuengzii/botio-livechat-backend/internal/db"
+	"github.com/Nhuengzii/botio-livechat-backend/internal/discord"
+	"github.com/Nhuengzii/botio-livechat-backend/pkg/stdmessage"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type Lambda struct {
 	config
 }
 
-func (l Lambda) handler(ctx context.Context, event events.SQSEvent) {
+type recievedSqsMessage struct {
+	Message string `json:"Message"`
+}
+
+var (
+	errUnmarshalRecievedBody    = errors.New("Error json unmarshal recieve body")
+	errUnmarshalRecievedMessage = errors.New("Error json unmarshal recieve message")
+)
+
+func (l Lambda) handler(ctx context.Context, sqsEvent events.SQSEvent) error {
+	var recieveBody recievedSqsMessage
+	var recieveMessage stdmessage.StdMessage
+	for _, record := range sqsEvent.Records {
+		err := json.Unmarshal([]byte(record.Body), &recieveBody)
+		if err != nil {
+			discord.Log(l.DiscordWebhookURL, "Error unmarshal recieveBody")
+			return errUnmarshalRecievedBody
+		}
+		err = bson.UnmarshalExtJSON([]byte(recieveBody.Message), true, &recieveMessage)
+		if err != nil {
+			return errUnmarshalRecievedMessage
+		}
+
+		// implement update conversation
+		// implement add message
+	}
+	return nil
 }
 
 func main() {
