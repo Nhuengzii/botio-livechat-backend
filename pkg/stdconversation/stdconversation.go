4e2e27f7c88612811bf7cb52f7d87bc95ef906ed
PAFF <jj2jom@gmail.com>
2023-06-08 14:09:41 +0700

    add pkg/stdconversation/stdconversation.go struct file

---
+++ pkg/stdconversation/stdconversation.go
@@ -0,0 +1,22 @@
+package stdconversation
+
+type StdConversation struct {
+	ShopID          string         `json:"shopID" bson:"shopID"`
+	PageID          string         `json:"pageID" bson:"pageID"`
+	ConversationID  string         `json:"conversationID" bson:"conversationID"`
+	ConversationPic Payload        `json:"conversationPic" bson:"conversationPic"`
+	UpdatedTime     int64          `json:"updatedTime" bson:"updatedTime"`
+	Participants    []*Participant `json:"participants" bson:"participants"`
+	LastActivity    string         `json:"lastActivity" bson:"lastActivity"`
+	IsRead          bool           `json:"isRead" bson:"isRead"`
+}
+
+type Participant struct {
+	UserID     string  `json:"userID" bson:"userID"`
+	Username   string  `json:"username" bson:"username"`
+	ProfilePic Payload `json:"profilePic" bson:"profilePic"`
+}
+
+type Payload struct {
+	Src string `json:"src" bson:"src"`
+}
