c842baec5a28183ecbde7caf1585d7c17b79fdd9
PAFF <jj2jom@gmail.com>
2023-06-08 18:37:49 +0700

    add transport response

--- internal/transport/lambda/response.go
+++ internal/transport/lambda/response.go
@@ -1,1 +1,10 @@
 package transport
+
+import "github.com/aws/aws-lambda-go/events"
+
+func SendResponse(statusCode int, body string) events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{
+		StatusCode: statusCode,
+		Body:       body,
+	}
+}
