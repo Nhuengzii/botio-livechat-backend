865e5cefc0d557447f6fab022c82da22437a0dc7
PAFF <jj2jom@gmail.com>
2023-06-08 14:22:14 +0700

    add internal/sqs/sqs.go a package for creating sqs client and sending message

---
+++ internal/sqs/sqs.go
@@ -0,0 +1,32 @@
+package sqs
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+type Client struct {
+	client *sqs.SQS
+}
+
+func NewClient() *Client {
+	sess := session.Must(session.NewSession())
+	client := sqs.New(sess, aws.NewConfig().WithRegion(os.Getenv("AWS_REGION")))
+	return &Client{client}
+}
+
+func (c *Client) SendMessage(queueURL string, message string) error {
+	input := &sqs.SendMessageInput{
+		MessageBody: aws.String(message),
+		QueueUrl:    aws.String(queueURL),
+	}
+	_, err := c.client.SendMessage(input)
+	if err != nil {
+		return fmt.Errorf("sqs.SendMessage: %w", err)
+	}
+	return nil
+}
