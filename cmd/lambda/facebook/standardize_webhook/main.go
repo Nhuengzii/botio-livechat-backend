018efdce3e6f9c466d416c2327f3c9e5b12e9162
PAFF <jj2jom@gmail.com>
2023-06-08 21:54:52 +0700

    create skeleton for main.go in standardize_webhook facebook

--- cmd/lambda/facebook/standardize_webhook/main.go
+++ cmd/lambda/facebook/standardize_webhook/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/Nhuengzii/botio-livechat-backend/internal/sqswrapper"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Lambda struct {
+	config
+}
+
+func (l Lambda) handler(ctx context.Context, event events.SQSEvent) {
+}
+
+func main() {
+	l := Lambda{
+		config: config{
+			DiscordWebhookURL: os.Getenv("DISCORD_WEBHOOK_URL"),
+			SqsQueueURL:       os.Getenv("SQS_QUEUE_URL"),
+			SqsClient:         *sqswrapper.NewClient(),
+		},
+	}
+	lambda.Start(l.handler)
+}
