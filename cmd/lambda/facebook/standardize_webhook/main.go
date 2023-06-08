51bdfe5c6a6532a5708f8304fe9cab8f5b372419
PAFF <jj2jom@gmail.com>
2023-06-08 22:23:00 +0700

    change recievemsg package path

--- cmd/lambda/facebook/standardize_webhook/main.go
+++ cmd/lambda/facebook/standardize_webhook/main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"os"
+	"time"
 
+	"github.com/Nhuengzii/botio-livechat-backend/internal/discord"
+	"github.com/Nhuengzii/botio-livechat-backend/internal/fbutil/recievemsg"
 	"github.com/Nhuengzii/botio-livechat-backend/internal/sqswrapper"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 type Lambda struct {
 	config
-}
-
-func (l Lambda) handler(ctx context.Context, event events.SQSEvent) {
 }
 
 func main() {
@@ -25,4 +28,28 @@ func main() {
 		},
 	}
 	lambda.Start(l.handler)
+}
+
+func (l Lambda) handler(ctx context.Context, sqsEvent events.SQSEvent) {
+	discord.Log(l.DiscordWebhookURL, "facebook standardize webhook handler")
+	start := time.Now()
+	var recieveMessage recievemsg.RecieveMessage
+	for _, record := range sqsEvent.Records {
+		err := json.Unmarshal([]byte(record.Body), &recieveMessage)
+		if err != nil || recieveMessage.Object != "page" {
+			discord.Log(l.DiscordWebhookURL, fmt.Sprintf("Error unknown webhook object: %v\n", err))
+			return
+		}
+		for _, message := range recieveMessage.Entry {
+			err = handleWebhookEntry(message)
+			if err != nil {
+				discord.Log(l.DiscordWebhookURL, fmt.Sprintf("Error handling webhook entry : %v", err))
+			}
+		}
+	}
+	discord.Log(l.DiscordWebhookURL, fmt.Sprintf("Elapsed: %v", time.Since(start)))
+}
+
+func handleWebhookEntry(message recievemsg.Notification) error {
+	return nil
 }
