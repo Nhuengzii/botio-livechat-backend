410e549abcdf53b838e420f7f0f2b50ab05ade1e
PAFF <jj2jom@gmail.com>
2023-06-08 22:38:41 +0700

    change standardize package path

--- cmd/lambda/facebook/standardize_webhook/main.go
+++ cmd/lambda/facebook/standardize_webhook/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"time"
 
 	"github.com/Nhuengzii/botio-livechat-backend/internal/discord"
-	"github.com/Nhuengzii/botio-livechat-backend/internal/fbutil/recievemsg"
+	"github.com/Nhuengzii/botio-livechat-backend/internal/fbutil/standardize"
 	"github.com/Nhuengzii/botio-livechat-backend/internal/sqswrapper"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -18,6 +19,11 @@ import (
 type Lambda struct {
 	config
 }
+
+var (
+	errNoMessageEntry     = errors.New("Error! no message entry")
+	errUnknownWebhookType = errors.New("Error! unknown webhook type found!")
+)
 
 func main() {
 	l := Lambda{
@@ -33,7 +39,7 @@ func main() {
 func (l Lambda) handler(ctx context.Context, sqsEvent events.SQSEvent) {
 	discord.Log(l.DiscordWebhookURL, "facebook standardize webhook handler")
 	start := time.Now()
-	var recieveMessage recievemsg.RecieveMessage
+	var recieveMessage standardize.RecieveMessage
 	for _, record := range sqsEvent.Records {
 		err := json.Unmarshal([]byte(record.Body), &recieveMessage)
 		if err != nil || recieveMessage.Object != "page" {
@@ -50,6 +56,22 @@ func (l Lambda) handler(ctx context.Context, sqsEvent events.SQSEvent) {
 	discord.Log(l.DiscordWebhookURL, fmt.Sprintf("Elapsed: %v", time.Since(start)))
 }
 
-func handleWebhookEntry(message recievemsg.Notification) error {
+func handleWebhookEntry(message standardize.Notification) error {
+	if len(message.MessageDatas) <= 0 {
+		return errNoMessageEntry
+	}
+
+	for _, messageData := range message.MessageDatas {
+		if messageData.Message.MessageID != "" {
+			// standardize messaging hooks
+			// var standardMessage stdmessage.StdMessage
+			// err := StandardizeMessage(messageData, message.PageID, &standardMessage)
+			// if err != nil {
+			// 	return err
+			// }
+		} else {
+			return errUnknownWebhookType
+		}
+	}
 	return nil
 }
