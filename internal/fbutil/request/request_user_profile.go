889fd405bc0ce107f49d255e918f08d62d906c2b
PAFF <jj2jom@gmail.com>
2023-06-09 15:43:05 +0700

    add request_user_profile function in request_user_profile

---
+++ internal/fbutil/request/request_user_profile.go
@@ -0,0 +1,33 @@
+package request
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func RequestFacebookUserProfile(access_token string, psid string) (ResponseFacebookUserProfile, error) {
+	uri := fmt.Sprintf("https://graph.facebook.com/%v?fields=name,profile_pic&access_token=%v", psid, access_token)
+
+	resp, err := http.Get(uri)
+	if err != nil {
+		return ResponseFacebookUserProfile{}, err
+	}
+	defer resp.Body.Close()
+
+	var body ResponseFacebookUserProfile
+	err = json.NewDecoder(resp.Body).Decode(&body)
+	if err != nil {
+		return ResponseFacebookUserProfile{}, err
+	}
+
+	return body, nil
+}
+
+type ResponseFacebookUserProfile struct {
+	Name       string `json:"name"`
+	ProfilePic string `json:"profile_pic"`
+	Locale     string `json:"locale"`
+	TimeZone   string `json:"timezone"`
+	Gender     string `json:"gender"`
+}
