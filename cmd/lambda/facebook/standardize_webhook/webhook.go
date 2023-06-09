2ddf5115bb1261e3640b8f7fc2efdd1872b76f95
PAFF <jj2jom@gmail.com>
2023-06-10 00:14:12 +0700

    rename webhook.go file and rename various struct in it

--- cmd/lambda/facebook/standardize_webhook/webhook.go
+++ cmd/lambda/facebook/standardize_webhook/webhook.go
@@ -1,17 +1,17 @@
 package main
 
 type ReceiveWebhook struct {
-	Object string         `json:"object"`
-	Entry  []Notification `json:"entry"`
+	Object  string  `json:"object"`
+	Entries []Entry `json:"entry"`
 }
 
-type Notification struct {
-	PageID       string        `json:"id"`
-	Time         int64         `json:"time"`
-	MessageDatas []MessageData `json:"messaging"`
+type Entry struct {
+	PageID     string      `json:"id"`
+	Time       int64       `json:"time"`
+	Messagings []Messaging `json:"messaging"`
 }
 
-type MessageData struct {
+type Messaging struct {
 	Sender    User     `json:"sender"`
 	Recipient User     `json:"recipient"`
 	Timestamp int64    `json:"timestamp"`
