5008e7f29e1f1f6c913092f3945375aedb4b4bad
Nhuengzii <32nhueng@gmail.com>
2023-06-09 12:15:38 +0700

    fixed `/{shop_id}` not being child of `/shops`

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_api_gateway_resource" "shops" {
 
 resource "aws_api_gateway_resource" "shop_id" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  parent_id   = aws_api_gateway_resource.shops.id
   path_part   = "{shop_id}"
 }
 
