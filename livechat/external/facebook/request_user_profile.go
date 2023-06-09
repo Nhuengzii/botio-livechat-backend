6d2d631d58b5ba759e4da182deda891ac6f13852
PAFF <jj2jom@gmail.com>
2023-06-09 23:03:51 +0700

    rename requestFacebookUserProfile struct

--- livechat/external/facebook/request_user_profile.go
+++ livechat/external/facebook/request_user_profile.go
@@ -6,25 +6,25 @@ import (
 	"net/http"
 )
 
-func RequestFacebookUserProfile(access_token string, psid string) (ResponseFacebookUserProfile, error) {
+func RequestFacebookUserProfile(access_token string, psid string) (FacebookUserProfile, error) {
 	uri := fmt.Sprintf("https://graph.facebook.com/%v?fields=name,profile_pic&access_token=%v", psid, access_token)
 
 	resp, err := http.Get(uri)
 	if err != nil {
-		return ResponseFacebookUserProfile{}, err
+		return FacebookUserProfile{}, err
 	}
 	defer resp.Body.Close()
 
-	var body ResponseFacebookUserProfile
+	var body FacebookUserProfile
 	err = json.NewDecoder(resp.Body).Decode(&body)
 	if err != nil {
-		return ResponseFacebookUserProfile{}, err
+		return FacebookUserProfile{}, err
 	}
 
 	return body, nil
 }
 
-type ResponseFacebookUserProfile struct {
+type FacebookUserProfile struct {
 	Name       string `json:"name"`
 	ProfilePic string `json:"profile_pic"`
 	Locale     string `json:"locale"`
