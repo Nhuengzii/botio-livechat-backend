72d3656d02d7677e429dec0fff6e6333596cd733
PAFF <jj2jom@gmail.com>
2023-06-09 15:43:30 +0700

    implemet userprofile struct in facebook stdconversation pkg

--- internal/fbutil/conversationfmt/conversationfmt.go
+++ internal/fbutil/conversationfmt/conversationfmt.go
@@ -1,25 +1,30 @@
 package conversationfmt
 
 import (
+	"github.com/Nhuengzii/botio-livechat-backend/internal/fbutil/request"
 	"github.com/Nhuengzii/botio-livechat-backend/pkg/stdconversation"
 	"github.com/Nhuengzii/botio-livechat-backend/pkg/stdmessage"
 )
 
 func NewStdConversation(facebookAccessToken string, message *stdmessage.StdMessage) (*stdconversation.StdConversation, error) {
+	userProfile, err := request.RequestFacebookUserProfile(facebookAccessToken, message.Source.UserID)
+	if err != nil {
+		return &stdconversation.StdConversation{}, err
+	}
 	newConversation := &stdconversation.StdConversation{
-		ShopID:          message.ShopID,
-		PageID:          message.PageID,
-		ConversationID:  message.ConversationID,
+		ShopID:         message.ShopID,
+		PageID:         message.PageID,
+		ConversationID: message.ConversationID,
 		ConversationPic: stdconversation.Payload{
-			// Src: ,
+			Src: userProfile.ProfilePic,
 		},
 		UpdatedTime: message.Timestamp,
 		Participants: []*stdconversation.Participant{
 			{
-				UserID: message.Source.UserID,
-				// Username: ,
+				UserID:   message.Source.UserID,
+				Username: userProfile.Name,
 				ProfilePic: stdconversation.Payload{
-					// Src: ,
+					Src: userProfile.ProfilePic,
 				},
 			},
 		},
