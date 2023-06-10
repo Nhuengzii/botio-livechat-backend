19a869747e815b7df0f4fbc86a64ba0561a90690
PAFF <jj2jom@gmail.com>
2023-06-10 14:31:56 +0700

    fb get_messages define config struct and create instance in main

--- cmd/lambda/facebook/get_messages/main.go
+++ cmd/lambda/facebook/get_messages/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/Nhuengzii/botio-livechat-backend/livechat/db/mongodb"
+)
+
+func main() {
+	dbClient, err := mongodb.NewClient(context.TODO(), &mongodb.Target{
+		URI:                     os.Getenv("DATABASE_CONNECTION_URI"),
+		Database:                "BotioLivechat",
+		CollectionMessages:      "facebook_messages",
+		CollectionConversations: "facebook_conversations",
+	})
+	if err != nil {
+		return
+	}
+	c := config{
+		DiscordWebhookURL: os.Getenv("DISCORD_WEBHOOK_URL"),
+		DbClient:          dbClient,
+	}
+}
