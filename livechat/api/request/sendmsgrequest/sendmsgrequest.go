d1601bee9d71f43cb89b625b1d6b3d8541588536
PAFF <jj2jom@gmail.com>
2023-06-10 17:31:56 +0700

    refactor response and request package create sendmsgresponse package

---
+++ livechat/api/request/sendmsgrequest/sendmsgrequest.go
@@ -0,0 +1,15 @@
+package sendmsgrequest
+
+type RequestMessage struct {
+	Message    string     `json:"message"`
+	Attachment Attachment `json:"attachment"`
+}
+
+type Attachment struct {
+	AttachmentType string      `json:"type"`
+	Payload        PayloadType `json:"payload"`
+}
+
+type PayloadType struct {
+	Src string `json:"src"`
+}
