a2f7b22c610b6c58aba34c826b8b1dd8b02d41f6
PAFF <jj2jom@gmail.com>
2023-06-10 18:07:26 +0700

    create fbrequest package's RequestPostMessage function

---
+++ livechat/external/fbrequest/request_post_message.go
@@ -0,0 +1,65 @@
+package fbrequest
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func RequestFacebookPostMessage(access_token string, fbRequest FBSendMsgRequest, pageID string, psid string) (*FBSendMsgResponse, error) {
+	uri := fmt.Sprintf("https://graph.facebook.com/v16.0/%v/messages?access_token=%v", pageID, access_token)
+
+	facebookReqBody, err := json.Marshal(fbRequest)
+	if err != nil {
+		return &FBSendMsgResponse{}, err
+	}
+
+	var response FBSendMsgResponse
+	resp, err := http.Post(uri, "application/json", bytes.NewReader(facebookReqBody))
+	if err != nil {
+		return &FBSendMsgResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	now := time.Now().UnixMilli()
+	err = json.NewDecoder(resp.Body).Decode(&response)
+
+	response.Timestamp = now
+	if err != nil {
+		return &FBSendMsgResponse{}, err
+	}
+	return &response, nil
+}
+
+type FBSendMsgResponse struct {
+	RecipientID string `json:"recipient_id"`
+	MessageID   string `json:"message_id"`
+	Timestamp   int64  `json:"timestamp"`
+}
+
+type FBSendMsgRequest struct {
+	Recipient     Recipient `json:"recipient"`
+	Message       any       `json:"message"`
+	MessagingType string    `json:"messaging_type"`
+}
+
+type MessageText struct {
+	Text string `json:"text"`
+}
+
+type MessageAttachment struct {
+	Attachment AttachmentFacebookRequest `json:"attachment"`
+}
+type AttachmentFacebookRequest struct {
+	AttachmentType string                    `json:"type"`
+	Payload        AttachmentFacebookPayload `json:"payload"`
+}
+type AttachmentFacebookPayload struct {
+	Src        string `json:"url"`
+	IsReusable bool   `json:"is_reusable"`
+}
+type Recipient struct {
+	Id string `json:"id"`
+}
