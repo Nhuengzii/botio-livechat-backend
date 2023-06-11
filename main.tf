c34ffcd5ef606186eaf712e147870fb2d584b8de
Nhuengzii <32nhueng@gmail.com>
2023-06-11 23:45:16 +0700

    use `line_rest_api` module in main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,34 @@ terraform {
   }
 }
 
+variable "line_channel_secret" {
+  type = string
+}
+
+variable "line_channel_access_token" {
+  type = string
+}
+
+variable "discord_webhook_url" {
+  type = string
+}
+
+variable "mongo_uri" {
+  type = string
+}
+
+variable "mongo_database" {
+  type = string
+}
+
+variable "mongo_collection_line_conversations" {
+  type = string
+}
+
+variable "mongo_collection_line_messages" {
+  type = string
+}
+
 provider "aws" {
   region = "ap-southeast-1"
 }
@@ -35,6 +63,21 @@ module "facebook_rest_api" {
   parent_id              = aws_api_gateway_resource.shop_id.id
   facebook_access_token  = var.facebook_access_token
   facebook_app_secret    = var.facebook_app_secret
+}
+
+module "line_rest_api" {
+  source                              = "./modules/line_rest_api"
+  platform                            = "line"
+  rest_api_id                         = aws_api_gateway_rest_api.rest_api.id
+  rest_api_execution_arn              = aws_api_gateway_rest_api.rest_api.execution_arn
+  parent_id                           = aws_api_gateway_resource.shop_id.id
+  line_channel_access_token           = var.line_channel_access_token
+  line_channel_secret                 = var.line_channel_secret
+  mongo_uri                           = var.mongo_uri
+  mongo_database                      = var.mongo_database
+  mongo_collection_line_messages      = var.mongo_collection_line_messages
+  mongo_collection_line_conversations = var.mongo_collection_line_conversations
+  discord_webhook_url                 = var.discord_webhook_url
 }
 
 resource "aws_api_gateway_deployment" "rest_api" {
