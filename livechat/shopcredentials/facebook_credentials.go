2e84c486307302567bed8725183faeea0c9176e0
PAFF <jj2jom@gmail.com>
2023-06-12 23:08:26 +0700

    remove various fields in facebookCredentials struct all that left is AccessToken

--- livechat/shopcredentials/facebook_credentials.go
+++ livechat/shopcredentials/facebook_credentials.go
@@ -1,7 +1,5 @@
 package shopcredentials
 
 type FacebookCredentials struct {
-	AccessToken         string `bson:"accessToken"`
-	AppSecret           string `bson:"appSecret"`
-	WebhookVerification string `bson:"webhookVerification"`
+	AccessToken string `bson:"accessToken"`
 }
