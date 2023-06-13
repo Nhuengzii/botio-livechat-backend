b0c11c467d94c06aea21e4855acd79134572b5c7
Nhuengzii <32nhueng@gmail.com>
2023-06-13 22:19:13 +0700

    move line_rest_api module variables to variables.tf

---
+++ modules/line_rest_api/variables.tf
@@ -0,0 +1,43 @@
+variable "platform" {
+  type = string
+}
+
+variable "rest_api_id" {
+  type = string
+}
+
+variable "rest_api_execution_arn" {
+  type = string
+}
+
+variable "line_channel_secret" {
+  type = string
+}
+
+variable "discord_webhook_url" {
+  type = string
+}
+
+variable "line_channel_access_token" {
+  type = string
+}
+
+variable "mongo_uri" {
+  type = string
+}
+
+variable "mongo_database" {
+  type = string
+}
+
+variable "mongo_collection_line_conversations" {
+  type = string
+}
+
+variable "mongo_collection_line_messages" {
+  type = string
+}
+
+variable "parent_id" {
+  type = string
+}
