7fb01f6283ee0ede0ebda46a2eb2cc840147ea8d
Nhuengzii <32nhueng@gmail.com>
2023-06-13 17:18:39 +0700

    add more method in cache interface

--- livechat/cache.go
+++ livechat/cache.go
@@ -6,4 +6,6 @@ type CacheClient interface {
 	Close(ctx context.Context) error
 	Set(ctx context.Context, key string, value string) error
 	Get(ctx context.Context, key string) (string, error)
+	GetConnectionIDs(ctx context.Context, shopID string) ([]string, error)
+	DeleteConnectionID(ctx context.Context, shopID string, connectionID string) error
 }
