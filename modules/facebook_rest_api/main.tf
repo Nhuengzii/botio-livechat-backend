4f6e1d50304611a5d16a92c21a39e7d5983fa27a
Nhuengzii <32nhueng@gmail.com>
2023-06-13 21:17:45 +0700

    move all variables in facebook_rest_api module to variables.tf

--- modules/facebook_rest_api/main.tf
+++ modules/facebook_rest_api/main.tf
@@ -7,42 +7,6 @@ terraform {
   }
 }
 
-variable "platform" {
-  type = string
-}
-
-variable "facebook_webhook_verification_string" {
-  type = string
-}
-
-variable "mongo_uri" {
-  type = string
-}
-variable "mongo_database" {
-  type = string
-}
-variable "discord_webhook_url" {
-  type = string
-}
-variable "rest_api_id" {
-  type = string
-}
-
-variable "rest_api_execution_arn" {
-  type = string
-}
-
-variable "facebook_access_token" {
-  type = string
-}
-
-variable "facebook_app_secret" {
-  type = string
-}
-
-variable "parent_id" {
-  type = string
-}
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
