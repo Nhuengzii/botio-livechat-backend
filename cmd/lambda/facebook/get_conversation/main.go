6a93f85ba6af6cae91f7eb0a4e4f2791e4fa786a
PAFF <jj2jom@gmail.com>
2023-06-14 16:47:37 +0700

    add skeleton for fb get-conversation main

--- cmd/lambda/facebook/get_conversation/main.go
+++ cmd/lambda/facebook/get_conversation/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/Nhuengzii/botio-livechat-backend/livechat/db/mongodb"
+	"github.com/Nhuengzii/botio-livechat-backend/livechat/discord"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func (c *config) handler(ctx context.Context, request events.APIGatewayProxyRequest) (_ events.APIGatewayProxyResponse, err error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "OK",
+	}, nil
+}
 
 func main() {
-	fmt.Println("Face book getCovneras ion")
+	ctx, cancel := context.WithTimeout(context.Background(), 2500*time.Millisecond)
+	defer cancel()
+
+	var (
+		mongodbURI        = os.Getenv("MONGODB_URI")
+		mongodbDatabase   = os.Getenv("MONGODB_DATABASE")
+		discordWebhookURL = os.Getenv("DISCORD_WEBHOOK_URL")
+	)
+	dbClient, err := mongodb.NewClient(ctx, mongodb.Target{
+		URI:                     mongodbURI,
+		Database:                mongodbDatabase,
+		CollectionMessages:      "messages",
+		CollectionConversations: "conversations",
+	})
+	c := config{
+		discordWebhookURL: discordWebhookURL,
+		dbClient:          dbClient,
+	}
+	if err != nil {
+		discord.Log(c.discordWebhookURL, fmt.Sprintln(err))
+		log.Fatalln(err)
+	}
+	defer func() {
+		discord.Log(c.discordWebhookURL, "defer dbClient close")
+		c.dbClient.Close(ctx)
+	}()
+
+	lambda.Start(c.handler)
 }
