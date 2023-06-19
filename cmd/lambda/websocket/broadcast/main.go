70d9f1ec0142dad5b88d2a96e9494df0c22297a8
Nhuengzii <32nhueng@gmail.com>
2023-06-19 23:43:12 +0700

    implement broadcast message

--- cmd/lambda/websocket/broadcast/main.go
+++ cmd/lambda/websocket/broadcast/main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/Nhuengzii/botio-livechat-backend/livechat/cache/redis"
+	"github.com/Nhuengzii/botio-livechat-backend/livechat/stdmessage"
+	"github.com/Nhuengzii/botio-livechat-backend/livechat/websocketwrapper"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
 func main() {
-	fmt.Println("broadcast handler")
+	addr := os.Getenv("REDIS_ADDR")
+	password := os.Getenv("REDIS_PASSWORD")
+	webhookURL := os.Getenv("DISCORD_WEBHOOK_URL")
+	websocket_api_id := os.Getenv("WEBSOCKET_API_ID")
+	cacheClient := redis.NewClient(addr, password)
+	websocketClient := websocketwrapper.NewClient(fmt.Sprintf("https://%s.execute-api.ap-southeast-1.amazonaws.com/dev", websocket_api_id))
+	if websocketClient == nil {
+		fmt.Println("websocketClient is nil")
+	}
+	c := Config{
+		cacheClient:       cacheClient,
+		webSocketClient:   websocketClient,
+		discordWebhookURL: webhookURL,
+	}
+	lambda.Start(c.Handler)
+}
+
+type ReceiveMessage struct {
+	Action  string                `json:"action"`
+	Message stdmessage.StdMessage `json:"message"`
+}
+
+func (c *Config) Handler(ctx context.Context, request events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
+	connectionID := request.RequestContext.ConnectionID
+	var receiveMessage ReceiveMessage
+	err := json.Unmarshal([]byte(request.Body), &receiveMessage)
+	if err != nil {
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+		}, nil
+	}
+	shopID := receiveMessage.Message.ShopID
+	connections, err := c.cacheClient.GetShopConnections(ctx, shopID)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, connection := range connections {
+		if connection != connectionID {
+			err := c.webSocketClient.Send(ctx, connection, request.Body)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+	}, nil
 }
