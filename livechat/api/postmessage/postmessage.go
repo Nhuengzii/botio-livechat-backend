653ddaf28802338312f94484b4bbb038c4d319aa
PAFF <jj2jom@gmail.com>
2023-06-19 23:51:22 +0700

    change payload template type to slice

--- livechat/api/postmessage/postmessage.go
+++ livechat/api/postmessage/postmessage.go
@@ -13,14 +13,14 @@ type Attachment struct {
 type Payload struct {
 	Src string `json:"src"`
 
-	FBTemplateButton                FBTemplateButton                `json:"fbTemplateButton"`
-	FBTemplateCoupon                FBTemplateCoupon                `json:"fbTemplateCoupon"`
-	FBTemplateCustomerFeedback      FBTemplateCustomerFeedback      `json:"fbTemplateCustomerFeedback"`
-	FBTemplateGeneric               FBTemplateGeneric               `json:"fbTemplateGeneric"`
-	FBTemplateMedia                 FBTemplateMedia                 `json:"fbTemplateMedia"`
-	FBTemplateProduct               FBTemplateProduct               `json:"fbTemplateProduct"`
-	FBTemplateReceipt               FBTemplateReceipt               `json:"fbTemplateReceipt"`
-	FBTemplateStructuredInformation FBTemplateStructuredInformation `json:"fbTemplateStructuredInformation"`
+	FBTemplateButton                []FBTemplateButton                `json:"fbTemplateButton"`
+	FBTemplateCoupon                []FBTemplateCoupon                `json:"fbTemplateCoupon"`
+	FBTemplateCustomerFeedback      []FBTemplateCustomerFeedback      `json:"fbTemplateCustomerFeedback"`
+	FBTemplateGeneric               []FBTemplateGeneric               `json:"fbTemplateGeneric"`
+	FBTemplateMedia                 []FBTemplateMedia                 `json:"fbTemplateMedia"`
+	FBTemplateProduct               []FBTemplateProduct               `json:"fbTemplateProduct"`
+	FBTemplateReceipt               []FBTemplateReceipt               `json:"fbTemplateReceipt"`
+	FBTemplateStructuredInformation []FBTemplateStructuredInformation `json:"fbTemplateStructuredInformation"`
 }
 
 type Response struct {
