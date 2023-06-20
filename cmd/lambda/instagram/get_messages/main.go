e20a2912ba6ba32324dc14e1ff02342c62af9ca1
PAFF <jj2jom@gmail.com>
2023-06-21 00:16:03 +0700

    finish ig get-messages

--- cmd/lambda/instagram/get_messages/main.go
+++ cmd/lambda/instagram/get_messages/main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"time"
 
+	"github.com/Nhuengzii/botio-livechat-backend/livechat/api/getmessages"
 	"github.com/Nhuengzii/botio-livechat-backend/livechat/db/mongodb"
 	"github.com/Nhuengzii/botio-livechat-backend/livechat/discord"
+	"github.com/Nhuengzii/botio-livechat-backend/livechat/stdmessage"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var (
+	errNoShopIDPath         = errors.New("err path parameter parameters shop_id not given")
+	errNoPageIDPath         = errors.New("err path parameter parameters page_id not given")
+	errNoConversationIDPath = errors.New("err path parameter conversation_id not given")
 )
 
 func (c *config) handler(ctx context.Context, request events.APIGatewayProxyRequest) (_ events.APIGatewayProxyResponse, err error) {
@@ -19,8 +29,102 @@ func (c *config) handler(ctx context.Context, request events.APIGatewayProxyRequ
 			discord.Log(c.discordWebhookURL, fmt.Sprintln(err))
 		}
 	}()
+	pathParams := request.PathParameters
+	shopID, ok := pathParams["shop_id"]
+	if !ok {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Bad Request",
+			Headers: map[string]string{
+				"Access-Control-Allow-Origin": "*",
+			},
+		}, errNoShopIDPath
+	}
+	pageID, ok := pathParams["page_id"]
+	if !ok {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Bad Request",
+			Headers: map[string]string{
+				"Access-Control-Allow-Origin": "*",
+			},
+		}, errNoPageIDPath
+	}
+	conversationID, ok := pathParams["conversation_id"]
+	if !ok {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Bad Request",
+			Headers: map[string]string{
+				"Access-Control-Allow-Origin": "*",
+			},
+		}, errNoConversationIDPath
+	}
 
-	return events.APIGatewayProxyResponse{}, nil
+	stdMessages := []stdmessage.StdMessage{}
+
+	filterQueryString, ok := request.QueryStringParameters["filter"]
+	if !ok {
+		stdMessages, err = c.dbClient.QueryMessages(ctx, shopID, pageID, conversationID)
+		if err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: 500,
+				Body:       "Internal Server Error",
+				Headers: map[string]string{
+					"Access-Control-Allow-Origin": "*",
+				},
+			}, err
+		}
+	} else {
+		var filter getmessages.Filter
+		err := json.Unmarshal([]byte(filterQueryString), &filter)
+
+		stdMessages, err = c.dbClient.QueryMessagesWithMessage(ctx, shopID, stdmessage.PlatformInstagram, pageID, conversationID, filter.Message)
+		if err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: 500,
+				Body:       "Internal Server Error",
+				Headers: map[string]string{
+					"Access-Control-Allow-Origin": "*",
+				},
+			}, err
+		}
+	}
+
+	getMessagesResponse := getmessages.Response{
+		Messages: stdMessages,
+	}
+
+	jsonBodyByte, err := json.Marshal(getMessagesResponse)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       "Internal Server Error",
+			Headers: map[string]string{
+				"Access-Control-Allow-Origin": "*",
+			},
+		}, err
+	}
+
+	if len(getMessagesResponse.Messages) != 0 {
+		err = c.dbClient.UpdateConversationIsRead(ctx, conversationID)
+		if err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: 502,
+				Body:       "Bad Gateway",
+				Headers: map[string]string{
+					"Access-Control-Allow-Origin": "*",
+				},
+			}, err
+		}
+	}
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(jsonBodyByte),
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin": "*",
+		},
+	}, nil
 }
 
 func main() {
