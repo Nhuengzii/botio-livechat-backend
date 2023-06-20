6f0aedd15d866eeca374976eb3cb0a5fec8c576e
PAFF <jj2jom@gmail.com>
2023-06-21 01:12:06 +0700

    add skeleton for get_conversation

--- cmd/lambda/instagram/get_conversation/main.go
+++ cmd/lambda/instagram/get_conversation/main.go
@@ -1,4 +1,62 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/Nhuengzii/botio-livechat-backend/livechat/db/mongodb"
+	"github.com/Nhuengzii/botio-livechat-backend/livechat/discord"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var (
+	errNoShopIDPath         = errors.New("err path parameter parameters shop_id not given")
+	errNoPageIDPath         = errors.New("err path parameter parameters page_id not given")
+	errNoConversationIDPath = errors.New("err path parameter parameters conversation_id not given")
+)
+
+func (c *config) handler(ctx context.Context, request events.APIGatewayProxyRequest) (_ events.APIGatewayProxyResponse, err error) {
+	defer func() {
+		if err != nil {
+			discord.Log(c.discordWebhookURL, fmt.Sprintln(err))
+		}
+	}()
+
+	return events.APIGatewayProxyResponse{}, nil
+}
+
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 2500*time.Millisecond)
+	defer cancel()
+
+	var (
+		mongodbURI        = os.Getenv("MONGODB_URI")
+		mongodbDatabase   = os.Getenv("MONGODB_DATABASE")
+		discordWebhookURL = os.Getenv("DISCORD_WEBHOOK_URL")
+	)
+	dbClient, err := mongodb.NewClient(ctx, mongodb.Target{
+		URI:                     mongodbURI,
+		Database:                mongodbDatabase,
+		CollectionMessages:      "messages",
+		CollectionConversations: "conversations",
+	})
+	c := config{
+		discordWebhookURL: discordWebhookURL,
+		dbClient:          dbClient,
+	}
+	if err != nil {
+		discord.Log(c.discordWebhookURL, fmt.Sprintln(err))
+		log.Fatalln(err)
+	}
+	defer func() {
+		discord.Log(c.discordWebhookURL, "defer dbClient close")
+		c.dbClient.Close(ctx)
+	}()
+
+	lambda.Start(c.handler)
 }
