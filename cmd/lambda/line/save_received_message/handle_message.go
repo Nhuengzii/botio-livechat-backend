f66d10c9bac4289e4fdf62822ec2d981c4989050
Narawit Sariddeechaikul <narawitpun@gmail.com>
2023-06-23 16:40:25 +0700

    use linebot.Client.GetProfile instead

--- cmd/lambda/line/save_received_message/handle_message.go
+++ cmd/lambda/line/save_received_message/handle_message.go
@@ -4,21 +4,22 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
 
 	"github.com/Nhuengzii/botio-livechat-backend/livechat/db/mongodb"
 	"github.com/Nhuengzii/botio-livechat-backend/livechat/stdmessage"
 )
 
-func (c *config) handleMessage(ctx context.Context, lineChannelAccessToken string, message *stdmessage.StdMessage) (err error) {
+func (c *config) handleMessage(ctx context.Context, bot *linebot.Client, message stdmessage.StdMessage) (err error) {
 	defer func() {
 		if err != nil {
 			err = fmt.Errorf("handleMessage: %w", err)
 		}
 	}()
-	err = c.dbClient.UpdateConversationOnNewMessage(ctx, message)
+	err = c.dbClient.UpdateConversationOnNewMessage(ctx, &message)
 	if err != nil {
 		if errors.Is(err, mongodb.ErrNoDocuments) {
-			conversation, err := newStdConversation(lineChannelAccessToken, message)
+			conversation, err := newStdConversation(bot, &message)
 			if err != nil {
 				return err
 			}
@@ -30,7 +31,7 @@ func (c *config) handleMessage(ctx context.Context, lineChannelAccessToken strin
 			return err
 		}
 	}
-	err = c.dbClient.InsertMessage(ctx, message)
+	err = c.dbClient.InsertMessage(ctx, &message)
 	if err != nil {
 		return err
 	}
