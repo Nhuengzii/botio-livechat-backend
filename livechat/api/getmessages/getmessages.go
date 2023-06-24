6e2ea4fd73cf12c4eb054b45fe8d22356ceb2702
PAFF <jj2jom@gmail.com>
2023-06-24 22:23:01 +0700

    add getmessages package doc

--- livechat/api/getmessages/getmessages.go
+++ livechat/api/getmessages/getmessages.go
@@ -1,13 +1,19 @@
+// Package getmessages define response body for rest api endpoints that return a list of stdmessage.StdMessage.
+//
+// The getmessages package should only be use to return response to the api caller.
 package getmessages
 
 import (
 	"github.com/Nhuengzii/botio-livechat-backend/livechat/stdmessage"
 )
 
+// A Response contains response body that should be return to the api caller.
 type Response struct {
+	// Messages store a slice of stdmessage.StdMessage struct define in package stdmessage.
 	Messages []stdmessage.StdMessage `json:"messages"`
 }
 
+// A Filter contains api request's query string parameters for filtering return result.
 type Filter struct {
-	Message string `json:"with_message"`
+	Message string `json:"with_message"` // filter messages that contains the text message
 }
