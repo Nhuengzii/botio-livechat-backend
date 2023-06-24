9b7e23b3fde71c4d200958e16681443afd5bf4a7
PAFF <jj2jom@gmail.com>
2023-06-24 22:33:53 +0700

    add patchconversation doc

--- livechat/api/patchconversation/patchconversation.go
+++ livechat/api/patchconversation/patchconversation.go
@@ -1,5 +1,9 @@
+// Package getpage define request body for rest api endpoints that return page's specific information
+//
+// The patchconversation package should only be use to read api request.
 package patchconversation
 
+// A Request contains request body of the api request.
 type Request struct {
-	Unread *int `json:"unread"`
+	Unread *int `json:"unread"` // number of unread messages in the conversation
 }
