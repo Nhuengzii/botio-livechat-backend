4e3401e9a11b30c2a34717df775a188d6902c90d
PAFF <jj2jom@gmail.com>
2023-06-24 22:29:10 +0700

    add getpage doc

--- livechat/api/getpage/getpage.go
+++ livechat/api/getpage/getpage.go
@@ -1,6 +1,10 @@
+// Package getpage define response body for rest api endpoints that return page's specific information
+//
+// The getpage package should only be use to return response to the api caller.
 package getpage
 
+// A Response contains response body that should be return to the api caller.
 type Response struct {
-	UnreadConversations int64 `json:"unreadConversations"`
-	AllConversations    int64 `json:"allConversations"`
+	UnreadConversations int64 `json:"unreadConversations"` // number of conversations that is not yet read by page admin
+	AllConversations    int64 `json:"allConversations"`    // number of page's total conversations
 }
