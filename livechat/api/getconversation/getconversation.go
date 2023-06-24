6f4e4657159824d510ab8f44062df59f26c0d16a
PAFF <jj2jom@gmail.com>
2023-06-24 22:11:15 +0700

    add getconversation package docs

--- livechat/api/getconversation/getconversation.go
+++ livechat/api/getconversation/getconversation.go
@@ -1,7 +1,12 @@
+// Package getconversation define response body for rest api endpoints that return a single stdconversation.StdConversation.
+//
+// The getconversation package should only be use to return response to the api caller.
 package getconversation
 
 import "github.com/Nhuengzii/botio-livechat-backend/livechat/stdconversation"
 
+// A Response contains response that should be return to the api caller.
 type Response struct {
+	// Conversation store a pointer to StdConversation struct define in package stdconversation.
 	Conversation *stdconversation.StdConversation `json:"conversation"`
 }
