5b1184d48b6bebe7a8ca397979a3a1e53347028d
PAFF <jj2jom@gmail.com>
2023-06-26 17:31:43 +0700

    finish snswrapper

--- livechat/snswrapper/snswrapper.go
+++ livechat/snswrapper/snswrapper.go
@@ -8,16 +8,20 @@ import (
 	"github.com/aws/aws-sdk-go/service/sns"
 )
 
+// A Client contains mongodb client and a Target struct.
 type Client struct {
-	client *sns.SNS
+	client *sns.SNS // SNS's client used to do various SNS's operation
 }
 
+// NewClient create and return a SNS's client.
 func NewClient(awsRegion string) *Client {
 	sess := session.Must(session.NewSession())
 	client := sns.New(sess, aws.NewConfig().WithRegion(awsRegion))
 	return &Client{client}
 }
 
+// PublishMessage recieve a message string and publish the message into specific SNS topic.
+// Return an error if it occurs.
 func (c *Client) PublishMessage(topicARN string, message string) error {
 	input := &sns.PublishInput{
 		Message:  aws.String(message),
@@ -30,6 +34,9 @@ func (c *Client) PublishMessage(topicARN string, message string) error {
 	return nil
 }
 
+// A SNSMessage contains an actual message recieved via SNS topic.
+//
+// This struct should be use to recieve and then unmarshal the actual message.
 type SNSMessage struct {
 	Message string `json:"message"`
 }
