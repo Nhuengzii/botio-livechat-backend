8ea75940fde6106f442f0b8430301035c8579d79
PAFF <jj2jom@gmail.com>
2023-06-27 19:17:39 +0700

    finish message_queue.go

--- livechat/message_queue.go
+++ livechat/message_queue.go
@@ -2,5 +2,7 @@ package livechat
 
 // MessageQueueClient is an interface for message queue client, for example, SQS
 type MessageQueueClient interface {
+	// SendMessage recieve a message string and send the message into specific message queue.
+	// Return an error if it occurs.
 	SendMessage(queueURL string, message string) error
 }
