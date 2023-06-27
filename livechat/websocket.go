33e47767d68f4e01afad8ab57eea190274481d55
PAFF <jj2jom@gmail.com>
2023-06-27 19:23:17 +0700

    finish websocket.go

--- livechat/websocket.go
+++ livechat/websocket.go
@@ -4,5 +4,7 @@ import "context"
 
 // WebsocketClient is an interface for websocket client, for example, API Gateway
 type WebsocketClient interface {
+	// Send recieve a message string and send the message into a specific websocket connection.
+	// Return an error if it occurs.
 	Send(ctx context.Context, connectionID string, message string) error
 }
