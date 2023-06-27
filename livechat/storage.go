574355f279bf65c6c2cfaaa41bbe1d5f942dd344
PAFF <jj2jom@gmail.com>
2023-06-27 19:22:40 +0700

    finish storage.go

--- livechat/storage.go
+++ livechat/storage.go
@@ -6,5 +6,9 @@ import (
 
 // StorageUploader is an interface for storage uploader, for example, S3
 type StorageUploader interface {
-	UploadFile(bucketName string, file io.Reader) (string, error)
+	// UploadFile upload input file into the specified storage and return a location string if the operations was a success.
+	// Return an error if it occurs.
+	//
+	// If implements by s3 storage name should be bucket name
+	UploadFile(storageName string, file io.Reader) (string, error)
 }
