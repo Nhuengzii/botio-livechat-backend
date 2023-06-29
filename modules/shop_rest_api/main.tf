78040f71528dd5fad82bf577f4dfa685650c4889
Nhuengzii <32nhueng@gmail.com>
2023-06-29 20:57:24 +0700

    fixed wrong parent_id in shop_rest_api module

--- modules/shop_rest_api/main.tf
+++ modules/shop_rest_api/main.tf
@@ -15,6 +15,6 @@ resource "aws_api_gateway_resource" "shops" {
 
 resource "aws_api_gateway_resource" "shop_id" {
   rest_api_id = var.rest_api_id
-  parent_id   = var.parent_resource_id
+  parent_id   = aws_api_gateway_resource.shops.id
   path_part   = "{shop_id}"
 }
