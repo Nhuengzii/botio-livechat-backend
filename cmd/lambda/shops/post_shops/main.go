e8aee0d92b93948cd29bec285d6ab3e913cd7469
Narawit Sariddeechaikul <narawitpun@gmail.com>
2023-07-01 12:33:30 +0700

    add skeleton for post_shops

--- cmd/lambda/shops/post_shops/main.go
+++ cmd/lambda/shops/post_shops/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"log"
+	"os"
+	"time"
+
+	"github.com/Nhuengzii/botio-livechat-backend/livechat/apigateway"
+
+	"github.com/Nhuengzii/botio-livechat-backend/livechat/db/mongodb"
+	"github.com/Nhuengzii/botio-livechat-backend/livechat/discord"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var errConversationNotExist = errors.New("err conversation does not exist")
+
+func (c *config) handler(ctx context.Context, req events.APIGatewayProxyRequest) (_ events.APIGatewayProxyResponse, err error) {
+	defer func() {
+		if err != nil {
+			logMessage := "cmd/lambda/line/post_shops/main.config.handler: " + err.Error()
+			log.Println(logMessage)
+			discord.Log(c.discordWebhookURL, logMessage)
+		}
+	}()
+	return apigateway.NewProxyResponse(200, "OK", "*"), nil
+}
 
 func main() {
-	fmt.Println("Hello, Wokr")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*2500)
+	defer cancel()
+	var (
+		mongodbURI        = os.Getenv("MONGODB_URI")
+		mongodbDatabase   = os.Getenv("MONGODB_DATABASE")
+		discordWebhookURL = os.Getenv("DISCORD_WEBHOOK_URL")
+	)
+	dbClient, err := mongodb.NewClient(ctx, mongodb.Target{
+		URI:                     mongodbURI,
+		Database:                mongodbDatabase,
+		CollectionConversations: "conversations",
+		CollectionMessages:      "messages",
+		CollectionShops:         "shops",
+	})
+	if err != nil {
+		logMessage := "cmd/lambda/line/post_shops/main.main: " + err.Error()
+		discord.Log(discordWebhookURL, logMessage)
+		log.Fatalln(logMessage)
+	}
+	defer dbClient.Close(ctx)
+	c := &config{
+		discordWebhookURL: discordWebhookURL,
+		dbClient:          dbClient,
+	}
+	lambda.Start(c.handler)
 }
