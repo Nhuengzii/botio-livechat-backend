40738ecb04119e2926036fdece7fd7bf7b7995d3
Nhuengzii <32nhueng@gmail.com>
2023-07-01 16:20:15 +0700

    add integration_http_method to module

--- modules/method_lambda_integration/main.tf
+++ modules/method_lambda_integration/main.tf
@@ -15,11 +15,12 @@ resource "aws_api_gateway_method" "method" {
 }
 
 resource "aws_api_gateway_integration" "integration" {
-  http_method = aws_api_gateway_method.method.http_method
-  rest_api_id = aws_api_gateway_method.method.rest_api_id
-  resource_id = aws_api_gateway_method.method.resource_id
-  type        = "AWS_PROXY"
-  uri         = var.lambda_invoke_arn
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  rest_api_id             = aws_api_gateway_method.method.rest_api_id
+  resource_id             = aws_api_gateway_method.method.resource_id
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_invoke_arn
 }
 
 resource "aws_lambda_permission" "endpoint_handler_permission" {
